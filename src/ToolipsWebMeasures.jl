2a6d68cffbe15c38a132cf49d9860534473a9f67
emmy <52672675+emmaccode@users.noreply.github.com>
2024-01-04 00:15:46 -0700

    (almost done)

--- src/ToolipsWebMeasures.jl
+++ src/ToolipsWebMeasures.jl
@@ -1,9 +1,26 @@
+"""
+#### ToolipsWebMeasures
+This module provides high-level measurement syntax for screen-based measurements.
+```julia
+using ToolipsWebMeasures
+
+mywidth = 50px
+mystuff = 30pt
+```
+For a list of measures, view `measures` documentation.
+"""
 module ToolipsWebMeasures
 import Base: *
 
 mutable struct WebMeasure{format} end
 
-*(i::Any, p::WebMeasure{<:Any}) = "$(i)$(typeof(p[1]).parameters[1])"
+*(i::Any, p::WebMeasure{<:Any}) = "$(i)$(typeof(p).parameters[1])"
+
+"""
+###### measures
+
+"""
+const measures = WebMeasure{:measure}()
 # size
 const px = WebMeasure{:px}()
 const pt = WebMeasure{:pt}()
@@ -16,22 +33,25 @@ const percent = WebMeasure{:%}()
 const per = WebMeasure{:%}()
 const em = WebMeasure{:em}()
 # time
-const seconds = WebMeasure{:seconds}()
-const s = WebMeasure{:seconds}()
+const seconds = WebMeasure{:s}()
+const s = WebMeasure{:s}()
 const ms = WebMeasure{:ms}()
 # angles
 const deg = WebMeasure{:deg}()
 const turn = WebMeasure{:turn}()
 # colors and transforms
-rgba(r::Number, g::Number, b::Number, a::Float64) = "rgb($r $g $b $a / a)"
+function rgba(r::Number, g::Number, b::Number, a::Float64)
+    "rgb($r $g $b $a / a)"
+end
+
 translateX(s::String) = "translateX($s)"
 translateY(s::String) = "translateX($s)"
 rotate(s::String) = "rotate($s)"
-matrix(n::Int64 ...) "matrix(" * join([string(i) for i in n], ", ") * ")"
+matrix(n::Int64 ...) = "matrix(" * join([string(i) for i in n], ", ") * ")"
 translate(x::String, y::String) = "translate()"
 skew(one::String, two::String) = "skew($one, $two)"
 scale(n::Any, n2::Any) = "scale($one, $two)"
 
-export px, pt, per, s, ms, deg, turn, 
+export px, pt, per, s, ms, deg, turn
 export rgba, translate, matrix, skew, rotate, scale
 end # module ToolipsWebMeasures
